0bb6b8df1b24b38e1155c4bb5a739be2230546b5
hritikRaheja <hritikraheja27@gmail.com>
2022-04-08 16:13:11 +0530

    Connected browser with blockchain using metemask and connected app with blockchain using web3.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
 
 class App extends Component {
+
+  async componentWillMount(){
+    await this.loadWeb3();
+    await this.loadBlockChain();
+  }
+
+  async loadBlockChain(){
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    // Setting the blockchain account address in state.
+    this.setState({accountAddress : accounts[0]});
+
+    const accountBalance = await web3.eth.getBalance(this.state.accountAddress);
+
+    // Setting the blockchain account balace in state.
+    this.setState({accountBalance : accountBalance});
+
+    //Getting the data from the state.
+    console.log(this.state.accountAddress);
+    console.log(this.state.accountBalance);
+  }
+
+  async loadWeb3(){
+      if(window.ethereum){
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      } else if(window.web3){
+        window.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        window.alert('Non-ethereum browser detected. You should consider trying MetaMask!')
+      }
+  }
+
+  constructor(props){
+    super(props);
+
+    // Setting the default state.
+    this.state = {
+      accountAddress : '',
+      accountBalance : '0'
+    }
+  }
+
   render() {
     return (
       <div>
@@ -25,20 +70,8 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Hello World!</h1>
               </div>
             </main>
           </div>
